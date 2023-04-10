99f9a2d4e1da9ab14c40c83ccdb019bca1d9ee51
Leo Franco <leonardo.franco@ges.inatel.br>
2023-04-10 14:08:19 -0300

    att

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,8 +1,6 @@
 ls
-cd S107-main
-ls
 cd Aula-GitHub-Actions
 ls
 sudo apt install mailutils
 echo "fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" mendoncawho@hotmail.com
+echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" mendoncawho@outlook.com
